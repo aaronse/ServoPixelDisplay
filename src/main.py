3d82f54a9775e0bc7e0d0bc75ddd4bee95ead202
aaron GitHub <aaronse@outlook.com>
2021-12-26 14:57:39 -0800

    Python implementation...

---
+++ src/main.py
@@ -0,0 +1,38 @@
+# *****************************************************************************
+#   "Multiverse's Lowest LO-FI Wood Pixel Display—Arduino Art" (Version 2.0)
+#   https://www.youtube.com/watch?v=...
+#
+#   This code was written to power the WTD 2.0 (Wood Tile Display) project 
+#   created after participating in Mark Rober's 
+#   https://monthly.com/mark-rober-engineering course.
+
+#   Consider buying drivers and other hardware goodies from the Adafruit 
+#   shop @ http://www.adafruit.com/products/815
+  
+#   Adafruit invests time and resources providing this open source code, 
+#   please support Adafruit and open-source hardware by purchasing 
+#   products from Adafruit!
+
+#   Cheers!
+
+#   aaron (Aza's Built to Code)
+# *****************************************************************************
+import argparse
+import sys
+import os
+from adafruit_servokit import ServoKit
+from adafruit_servokit import PCA9685
+from time import sleep
+
+
+parser = argparse.ArgumentParser(add_help=False)
+
+# try:
+
+
+# except KeyboardInterrupt:
+print('\nDone')
+#     parser.exit(0)
+# except Exception as e:
+#     parser.exit(type(e).__name__ + ': ' + str(e))
+
